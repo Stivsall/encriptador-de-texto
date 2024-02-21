bd0c52f235c397616f3691d3e2ee0c864ddf4ac6
Stiven La Rotta <stivsall@msn.com>
2024-02-21 11:16:51 -0500

    inicia codificación

---
+++ encripta.js
@@ -0,0 +1,6 @@
+console.log("Hola mundo");
+
+//constantes que llaman al id del index
+const campoTexto = document.querySelector("#texto-encriptado");
+const campoEncriptado = document.querySelector("#envia-texto-encriptado");
+
